84481db28019da624312bbf6a65b727c4bebbd31
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-08-09 10:32:27 +0100

    Create spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+// import "./SplashScreen.css";
+import { ReactComponent as SpinnerSvg } from "../../src/icons/real_spinner.svg";
+
+function Spinner() {
+  return (
+    <div>
+      <SpinnerSvg />
+    </div>
+  );
+}
+
+export default Spinner;
