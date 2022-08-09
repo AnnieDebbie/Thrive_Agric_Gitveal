98a3cf1749607fd149e0b30b7082e48bcf67392a
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-08-09 10:37:53 +0100

    Add search input and repository items components to app root

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import SearchInput from "./components/SearchInput";
+import { RepositoryProvider } from "./store/RepositoryContext";
+import RepositoryItems from "./components/RepositoryItems";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RepositoryProvider>
+        <SearchInput />
+        <RepositoryItems />
+      </RepositoryProvider>
     </div>
   );
 }
