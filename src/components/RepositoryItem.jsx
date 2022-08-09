7a20dba53a351601c88d74ee8462a17dd1bd0263
AnnieDebbie <32017636+AnnieDebbie@users.noreply.github.com>
2022-08-09 15:37:03 +0100

    Change class to className

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -8,23 +8,23 @@ const RepositoryItem = (props) => {
   if (daysPassed === 0) return 'Today';
     if (daysPassed === 1) return 'Yesterday';
       if (daysPassed <= 7) return `${daysPassed} days ago`;
-            if (daysPassed <=30) return 'this month';
+            if (daysPassed <=30) return 'This month';
       else {
           return new Intl.DateTimeFormat(locale).format(date);
             }
             };
   return (
       <div className="list-group">
-            <div class="list-group-item list-group-item-action" aria-current="true">
+            <div className="list-group-item list-group-item-action" aria-current="true">
                     <div>
                               <a href={html_url}>
                                           <h5 class="mb-1">{name}</h5>
                                                     </a>
-                                                              <div class="d-flex w-100 justify-content-between">
-                                                                          <span class="badge bg-primary rounded-pill"> {props.private ? "private" : "public"}</span>
+                                                              <div className="d-flex w-100 justify-content-between">
+                                                                          <span className="badge bg-primary rounded-pill"> {props.private ? "private" : "public"}</span>
           </div>
                   </div>
-        <p class="mb-1">{description}</p>
+        <p className="mb-1">{description}</p>
                 <div className  ="d-flex w-100">
                         <small> { formatDate(updated_at, en-US)}</small>
                                 <small> {language}</small>
